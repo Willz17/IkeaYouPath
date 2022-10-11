7e2b757143aff5f8bb35aa512c1dc07f100bcce1
Elev6477! <wills.ekanem@publicissapient.com>
2022-10-11 12:02:58 +0200

    added dotenv and mongoDB dep

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const products_router = require("./routes/products-router");
@@ -7,6 +10,18 @@ const cart_router = require("./routes/cart-router");
 const PORT = 4000;
 const app = express();
 app.use(express.json());
+
+// mongoose.connect(process.env.DB_URL);
+
+// const db = mongoose.connection;
+
+// db.on("error", (error) => {
+//   console.error(error);
+// });
+
+// db.once("open", () => {
+//   console.log("Connected to DB");
+// });
 
 // 'api/products'
 app.use("/api/products", products_router);
