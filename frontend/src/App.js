addf8d9ea350377959372f0b3e1c05afd69ef5c0
MarlonPublicis <113995333+MarlonPublicis@users.noreply.github.com>
2022-10-11 11:26:01 +0200

    adding components folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import NewSearch from "./components/NewSearch"
 
 function App() {
   return (
     <div className="App">
       <h4>Nordics Team</h4>
+      <NewSearch />
     </div>
   );
 }
