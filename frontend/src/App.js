620399e5e3d82f64a27da8b4f66bb24a8b5f2ad0
Eduard Sinyol <edusinyo@publicisgroupe.net>
2022-10-12 10:59:42 +0200

    cart things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Cart from "./components/Cart";
+import React, { useState } from "react";
 
 function App() {
+  const ItemList = [
+    { name: "Cool Knife", section: 4, price: 35, number: 1, coordinates: {x:50, y:30} },
+    { name: "Lamp", section: 3, price: 27, number: 1, coordinates: {x:70, y:90} },
+    { name: "Table", section: 1, price: 55, number: 2, coordinates: {x:60, y:76} },
+    { name: "Sofa", section: 4, price: 40, number: 1, coordinates: {x:78, y:20} },
+  ];
+
+  const [items,setItems] = useState(ItemList);
+
   return (
     <div className="App">
-      <h4>Nordics Team</h4>
+      <Cart item={items} />
     </div>
   );
 }
---
+++ frontend/src/components/Cart.js
@@ -0,0 +1,36 @@
+import "./Cart.css";
+import CartItems from "./CartItems.js";
+import React, { useState } from "react";
+import "./Buttons.css";
+
+function Cart(props) {
+  const [itemObject, setItemObject] = useState("");
+  let FinalPrice = 0;
+  let ActualPrice;
+  //console.log(ActualPrice)
+  //FinalPrice = (FinalPrice + ActualPrice);
+  return (
+    <div>
+      <div>
+        <h1>CART</h1>
+      </div>
+      <div className="ListText">
+        {props.item.map((listItem) => (
+          <CartItems
+            name={listItem.name}
+            section={listItem.section}
+            price={listItem.price}
+            coordinates={listItem.coordinates}
+            number={listItem.number}
+          />
+        ))}
+        <br></br>
+        Final Price: <label>{FinalPrice}</label>
+        <br></br>
+        <button className="SubmitButton">CHECKOUT</button>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
