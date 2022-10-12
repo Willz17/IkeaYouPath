6c7c29285c4a6bf5396d952d5cfff325185bdc30
MarlonPublicis <113995333+MarlonPublicis@users.noreply.github.com>
2022-10-12 18:17:03 +0200

    Fixed search button for fetching items

--- frontend/src/components/NewList.js
+++ frontend/src/components/NewList.js
@@ -31,9 +31,11 @@ const productList = [
 const NewList = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [result, setResults] = useState();
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
+
   var itemName = [];
   var itemPrice = [];
   var itemSection = [];
@@ -50,19 +52,79 @@ const NewList = () => {
     setSearchResults(results);
   }, [searchTerm]);
 
+  const showItems = (event) => {
+    console.log(searchTerm);
+    //get data from API with searchTerm as input, productList = get(searchTerm)
+    event.preventDefault();
+    var itemName = [];
+    var itemPrice = [];
+    var itemSection = [];
+    productList.forEach((obj) => {
+      itemName.push(obj.name);
+      itemPrice.push(obj.price);
+      itemSection.push(obj.section);
+    });
+    let solution;
+    solution = itemName.map((item, index) => (
+        <Card className="p-2" bg="light" style={{ width: "auto" }}>
+          <Row className="align-items-center">
+            <div className="col-md-3">
+              <img
+                src="https://media.istockphoto.com/photos/chefs-kitchen-knife-picture-id1092668906?k=20&m=1092668906&s=612x612&w=0&h=IzaoUoNJXe8P7FkB4MFKO09r0FVMlXvjfdabFmegCkI="
+                alt="..."
+                class="img-thumbnail"
+              ></img>
+            </div>
+            <div className="col-md-7">
+              {itemName[index]} <p>{"Price: " + itemPrice[index]}</p>{" "}
+              <p>{"Section: " + itemSection[index]}</p>
+            </div>
+
+            <div className="col-md-2">
+              <Button className="pull-right" variant="primary">
+                Add to Shoppinglist
+              </Button>{" "}
+            </div>
+          </Row>
+        </Card>
+      ))
+    setResults(solution);
+  };
   return (
     <div>
       <Row className="align-items-center">
-        <input
-          className="form-control p-2"
+        {/* <input
+          className="form-control "
           type="text"
           placeholder="Search for Item..."
           value={searchTerm}
           onChange={handleChange}
-        />
+        /> */}
+        <Container>
+          <div>
+          <form onSubmit={showItems} class="input-group mb-3 mt-5 w-75 center">
+            <input
+              type="text"
+              className ="form-control"
+              placeholder="Search for Item..."
+              onChange={event => setSearchTerm(event.target.value)}
+              value={searchTerm}
+            />
+            <div class="input-group-append">
+              <Button
+                className ="btn btn-outline"
+                type="submit"
+              >
+                Search
+              </Button>
+            </div>
+            </form>
+          </div>
+        </Container>
       </Row>
-
-      {searchResults.map((item, index) => (
+      
+      {result}
+      {/* {searchResults.map((item, index) => (
         <Card className="p-2" bg="light" style={{ width: "auto" }}>
           <Row className="align-items-center">
             <div className="col-md-3">
@@ -79,12 +141,12 @@ const NewList = () => {
 
             <div className="col-md-2">
               <Button className="pull-right" variant="primary">
-                Add to Cart
+                Add to Shoppinglist
               </Button>{" "}
             </div>
           </Row>
         </Card>
-      ))}
+      ))} */}
     </div>
   );
 };
