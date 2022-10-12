1c06b4d63e9b86d5c4c0972f2d7f51fff272b335
MarlonPublicis <113995333+MarlonPublicis@users.noreply.github.com>
2022-10-12 14:22:24 +0200

    Fixed bootstraped buttons and image for items

--- frontend/src/components/NewList.js
+++ frontend/src/components/NewList.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 
-import "./NewList.css"
-import Card from "./Card";
+import "./NewList.css";
+// import Card from "./Card";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 const productList = [
-    {
-        name: "First knife",
-        price: "20€",
-        section: "Kitchen",
-        coordinates: {x:50, y:20}
-    },
-    {
-        name: "Second knife",
-        price: "15€",
-        section: "Knives",
-        coordinates: {x:50, y:20}
-    },
-    {
-        name: "Third knife",
-        price: "30€",
-        section: "Knives",
-        coordinates: {x:50, y:20}
-    }
+  {
+    name: "First knife",
+    price: "20€",
+    section: "Kitchen",
+    coordinates: { x: 50, y: 20 },
+  },
+  {
+    name: "Second knife",
+    price: "15€",
+    section: "Knives",
+    coordinates: { x: 50, y: 20 },
+  },
+  {
+    name: "Third knife",
+    price: "30€",
+    section: "Knives",
+    coordinates: { x: 50, y: 20 },
+  },
 ];
-
 
 const NewList = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -38,8 +41,8 @@ const NewList = () => {
     itemName.push(obj.name);
     itemPrice.push(obj.price);
     itemSection.push(obj.section);
-  })
-  
+  });
+
   React.useEffect(() => {
     const results = itemName.filter((person) =>
       person.toLowerCase().includes(searchTerm.toLowerCase())
@@ -49,24 +52,41 @@ const NewList = () => {
 
   return (
     <div>
-      <input className="search_form"
-        type="text"
-        placeholder="Search for Item..."
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      
-        {searchResults.map((item, index) => (
-          <Card><li className="each_item">
-            {item} <p className="price_tag">{"Price: "+itemPrice[index]}</p> <p className="section_tag">{"Section: "+itemSection[index]}</p>
-          </li>
-          <p><button type="submit" className="add_to_cart">Add to cart</button></p>
-          
-          </Card>
-        ))}
+      <Row className="align-items-center">
+        <input
+          className="form-control p-2"
+          type="text"
+          placeholder="Search for Item..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </Row>
+
+      {searchResults.map((item, index) => (
+        <Card className="p-2" bg="light" style={{ width: "auto" }}>
+          <Row className="align-items-center">
+            <div className="col-md-3">
+              <img
+                src="https://media.istockphoto.com/photos/chefs-kitchen-knife-picture-id1092668906?k=20&m=1092668906&s=612x612&w=0&h=IzaoUoNJXe8P7FkB4MFKO09r0FVMlXvjfdabFmegCkI="
+                alt="..."
+                class="img-thumbnail"
+              ></img>
+            </div>
+            <div className="col-md-7">
+              {item} <p>{"Price: " + itemPrice[index]}</p>{" "}
+              <p>{"Section: " + itemSection[index]}</p>
+            </div>
+
+            <div className="col-md-2">
+              <Button className="pull-right" variant="primary">
+                Add to Cart
+              </Button>{" "}
+            </div>
+          </Row>
+        </Card>
+      ))}
     </div>
   );
 };
 
 export default NewList;
-
