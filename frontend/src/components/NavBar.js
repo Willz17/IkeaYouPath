d6be29b19cee34daa1230fbb8c3fdecf1c6b69d3
Lutfi Haji Cheteh <luthajic@publicisgroupe.net>
2022-10-13 11:33:44 +0200

    added home page with 2 options

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,11 +6,11 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href='/'>IKEA YouPath</Navbar.Brand>
+        <Navbar.Brand href='/home'>IKEA YouPath</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/home'>Home</Nav.Link>
             <Nav.Link href='/search'>Search</Nav.Link>
             <Nav.Link href='/cart'>Cart</Nav.Link>
             <Nav.Link href='/account'>Account</Nav.Link>
