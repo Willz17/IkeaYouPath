80c6c4a92f66f9cacf188279221dce96fc194355
Ted Dahlqvist <ted.dahlqvist@publicissapient.com>
2022-10-16 22:41:33 +0200

    +2 items / copy paste login
    Added two items according to the wishes of Kajsa. Copy pasted the signup page to the login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,33 +1,52 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Margin.css";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const LOGIN_URL = "http://localhost:4000/api/users/login";
+  // const [userData, setState] = useState[{}];
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = (event) => {
+    console.log(event);
+    event.preventDefault();
+    console.log(email);
+    const request_data = {
+      email: email,
+      password: password,
+    };
+
+    axios.post(LOGIN_URL, request_data, { mode: "no-cors" });
+  };
+
   return (
     <div>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-        </Form.Text>
-      </Form.Group>
+      <Form onSubmit={loginUser}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Remember me" />
-      </Form.Group>
-      
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-      <Button variant="link" type="button" href="/signup">
-        Signup
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remember me" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" onSubmit={loginUser}>
+          Login
+        </Button>
+        <Button variant="link" type="button" href="/signup">
+          Signup
+        </Button>
+      </Form>
     </div>
   );
 };
