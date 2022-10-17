9e373693354c6ede5e6781f5e1123427bf874844
TedDahlqvistPublicisSapient <ted.dahlqvist@publicissapient.com>
2022-10-17 11:54:07 +0200

    Login API call

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,47 +6,75 @@ import axios from "axios";
 
 const Login = () => {
   const LOGIN_URL = "http://localhost:4000/api/users/login";
-  // const [userData, setState] = useState[{}];
-
+  
+ 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [response, setResponse] = useState({})
 
-  const loginUser = (event) => {
+const loginUser = (event) => {
     console.log(event);
     event.preventDefault();
-    console.log(email);
+    console.log(email, password);
     const request_data = {
       email: email,
       password: password,
     };
 
-    axios.post(LOGIN_URL, request_data, { mode: "no-cors" });
+    axios.post(LOGIN_URL, request_data, { mode: "no-cors" }).then((res) =>{
+      setResponse(res.data)
+      
+      let data = response[0]
+      data = {
+        email: data.email,
+        userID: data.id
+      }
+
+      console.log(data);
+      saveToLocale(data)
+    }).catch((error)=>{
+      console.log(error);
+    })
+    
   };
 
+  const saveToLocale = ({email, userID}) =>{
+    let data = {
+      email: email,
+      userID: userID
+    }
+    localStorage.setItem('cred', JSON.stringify(data))
+  }
+  
+  
+  
   return (
     <div>
-      <Form onSubmit={loginUser}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted"></Form.Text>
-        </Form.Group>
+    <Form onSubmit={loginUser}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email"  
+        onChange={(event) => setEmail(event.target.value) }/>
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit" onSubmit={loginUser}>
-          Login
-        </Button>
-        <Button variant="link" type="button" href="/signup">
-          Signup
-        </Button>
-      </Form>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password"   
+        onChange={(event) => setPassword(event.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Remember me" />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit" onSubmit={loginUser}>
+        Login
+      </Button>
+      <Button variant="link" type="button" href="/signup">
+        Signup
+      </Button>
+    </Form>
     </div>
   );
 };
